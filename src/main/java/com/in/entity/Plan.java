3e651754940b713a52947ba2b57275af9688f1f6
anwar028 <anwarmohd131@gmail.com>
2025-06-24 17:13:53 +0530

    Token 121 is fixed

--- src/main/java/com/in/entity/Plan.java
+++ src/main/java/com/in/entity/Plan.java
@@ -1,7 +1,17 @@
 package com.in.entity;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+
 // Token 121
+@Entity
+@Data
 public class Plan {
+	
+	private Integer planId;
+	private String planName;
+	private String planStatus;
+
 	
 
 
